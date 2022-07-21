b1ded6a92e040f629ed1410ea5dd6b8e685c2b27
kev <kev.hannen@gmail.com>
2022-07-21 11:07:02 +0100

    added timer triggered function

---
+++ src/Local.Functions/TimerTriggeredFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Local.Functions
+{
+    public static class TimerTriggeredFunction
+    {
+        [FunctionName("TimerTriggeredFunction")]
+        public static async Task RunAsync([TimerTrigger("0 */1 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
+            
+        }
+    }
+}
